b488c4bedef4ca41a14233f643747935916dcb5a
domschoen <dschoenenberger@mac.com>
2020-11-16 16:18:47 +0100

    setup of automatic Enzyme toJSON

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,13 +1,12 @@
 //react-test-renderer
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
     const wrapper = shallow(<Header />);
-    expect(toJSON(wrapper)).toMatchSnapshot();
-
+    expect(wrapper).toMatchSnapshot();
+ 
 //    expect(wrapper.find('h1').length).toBe(1);
 //    expect(wrapper.find('h1').text()).toBe('Expensify');
 
