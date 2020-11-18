435b8a060ab54891479fa9b26615429c69f0dd86
domschoen <dschoenenberger@mac.com>
2020-11-18 16:06:33 +0100

    Testing implemented

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,37 +1,57 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { connect } from 'react-redux';
 import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-    return (
-        <div>
-            <ExpenseForm
-                expense={props.expense}
-                onSubmit={(expense) => {
-                    // Dispatch the action to edit the expense
-                    // Redirect to the dashboard
+// Refactor EditExpensePage to be a class based component
+// Setup mapDispatchToProps editExpense and removeExpense (in order to use spies)
 
-                    props.dispatch(editExpense(props.expense.id, expense));
-                    props.history.push('/');
+// Test:
+// should render EditExpensePage
+// snapshot
 
-                    console.log('updated',expense);
-                }}
-            />
-            <button onClick={() => {
-                        props.dispatch(removeExpense({ id: props.expense.id }));
-                        props.history.push('/');
-                    }}>Remove</button>
-        </div>
-    );
-};
-// Remove expense via dispatch and then redirect to dashboard
+// should handle editExpense
+// spies
 
-const mapStateToProps = (state, props) => {
-    return {
-        expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+// should handle removeExpense
+// spies
+
+export class EditExpensePage extends React.Component {
+    onSubmit = (expense) => {
+        // Dispatch the action to edit the expense
+        // Redirect to the dashboard
+
+        this.props.editExpense(this.props.expense.id, expense);
+        this.props.history.push('/');
+
+        console.log('updated',expense);
     };
+    onRemove = () => {
+        this.props.removeExpense({ id: this.props.expense.id });
+        this.props.history.push('/');
+    };
+    render() {
+        return (
+            <div>
+                <ExpenseForm
+                    expense={this.props.expense}
+                    onSubmit={this.onSubmit}
+                />
+                <button onClick={this.onRemove}>Remove</button>
+            </div>
+        );
+    }
 };
 
-export default connect(mapStateToProps)(EditExpensePage);
+// Remove expense via dispatch and then redirect to dashboard
+const mapStateToProps = (state, props) => ({
+   expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+});
+
+const mapDispatchToProps = (dispatch, props) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  removeExpense: (id) => dispatch(removeExpense(id))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
