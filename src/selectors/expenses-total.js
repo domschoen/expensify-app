2722a14abf1f0e6f29a9eb65c52ff6d1e1df7e18
domschoen <dschoenenberger@mac.com>
2020-11-24 16:31:37 +0100

    Add expense summary to dashboard

---
+++ src/selectors/expenses-total.js
@@ -0,0 +1,8 @@
+
+
+// Get expenses total
+export default (expenses) => {
+    return expenses
+                .map((expense) => expense.amount)
+                .reduce((sum, value) => sum + value, 0);
+};
